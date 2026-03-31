e3de28fe5684233f62913e18864113eb046372a8
AugustoLeo2010 <augustoleo.neres@gmail.com>
2026-03-30 22:12:17 -0300

    mais_menos
    Implement main method to read three numbers and calculate their sum and average.

---
+++ maior_menor.java
@@ -0,0 +1,11 @@
+ public class maior_menor {
+ void main() {
+
+ float N1 = Float.parseFloat(IO.readln("Digite o primeiro numero: "));
+ float N2 = Float.parseFloat(IO.readln("Digite o segundo numero: "));
+ float N3 = Float.parseFloat(IO.readln("Digite o terceiro numero: "));
+ float Soma = (N1 + N2 + N3);  float Media = (N1 + N2 + N3) / 3;
+
+}
+}
+    
