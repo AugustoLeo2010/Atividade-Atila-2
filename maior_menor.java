ad227f73e3dd5994960ec87788924115fa31098a
AugustoLeo2010 <augustoleo.neres@gmail.com>
2026-03-31 20:47:37 -0300

    maior_menor

--- maior_menor.java
+++ maior_menor.java
@@ -1,11 +1,36 @@
- public class maior_menor {
- void main() {
+public class maior_menor {
+    public static void main(String[] args) {
 
- float N1 = Float.parseFloat(IO.readln("Digite o primeiro numero: "));
- float N2 = Float.parseFloat(IO.readln("Digite o segundo numero: "));
- float N3 = Float.parseFloat(IO.readln("Digite o terceiro numero: "));
- float Soma = (N1 + N2 + N3);  float Media = (N1 + N2 + N3) / 3;
+       int a,b, c;
+       int maior,menor;
+       
+        float N1 = Float.parseFloat(IO.readln("Digite o primeiro numero: "));
+        float N2 = Float.parseFloat(IO.readln("Digite o segundo numero: "));
+        float N3 = Float.parseFloat(IO.readln("Digite o terceiro numero: "));
+        float Soma = (N1 + N2 + N3);
+        float Media = (N1 + N2 + N3) / 3;
 
-}
+        menor = a;
+        maior = a;
+
+        if (b < menor) {
+            menor = b;
+        }
+        if (c < menor) {
+            menor = c;
+        }
+        if (b > maior) {
+            maior = b;
+        }
+        if (c > maior) {
+            maior = c;
+        }
+
+       IO.println("Menor número: " + menor);
+        IO.println("Maior número: " + maior);
+        IO.println("O valor da soma é:" + Soma );
+        IO.println("A Media será de:" + Media);
+
+    }
 }
     
