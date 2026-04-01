895b2f39ca92bfb59496b5df607bb90cb2cc3960
AugustoLeo2010 <augustoleo.neres@gmail.com>
2026-04-01 16:41:33 -0300

    maior_menor
    Refactor to determine the largest and smallest of three numbers with handling for equal values.

--- maior_menor.java
+++ maior_menor.java
@@ -1,29 +1,31 @@
 public class maior_menor {
     public static void main(String[] args) {
 
-       int a,b, c;
-       int maior,menor;
-       
         float N1 = Float.parseFloat(IO.readln("Digite o primeiro numero: "));
         float N2 = Float.parseFloat(IO.readln("Digite o segundo numero: "));
         float N3 = Float.parseFloat(IO.readln("Digite o terceiro numero: "));
         float Soma = (N1 + N2 + N3);
         float Media = (N1 + N2 + N3) / 3;
 
-        menor = a;
-        maior = a;
-
-        if (b < menor) {
-            menor = b;
+        if (N1 == N2 && N1 == N3) {
+            IO.println("Ambos sao iguais");
+            IO.println("O valor da soma é:" + Soma );
+            IO.println("A Media será de:" + Media);
+        } else { 
+         int maior = N1;
+         int menor = N1;
+            
+        if (N2 < menor) {
+            menor = N2;
         }
-        if (c < menor) {
-            menor = c;
+        if (N3 < menor) {
+            menor = N3;
         }
-        if (b > maior) {
-            maior = b;
+         if (N2 > maior) {
+            maior = N2;
         }
-        if (c > maior) {
-            maior = c;
+        if (N3 > maior) {
+            maior = N3;
         }
 
        IO.println("Menor número: " + menor);
